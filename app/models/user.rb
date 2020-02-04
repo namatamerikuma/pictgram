faca20b4b48148c9f9b4f841f0f546a783b57c56
namatamerikuma <lshengtianmu55@gmail.com>
2020-02-04 14:14:00 +0900

    No.18 task

---
+++ app/models/user.rb
@@ -0,0 +1,20 @@
+class User < ApplicationRecord
+	VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])[a-z\d]{8,32}+\z/
+	VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/
+
+	validates :name, 
+	          presence: true,
+	          length: { maximum: 15}
+
+	validates :email,
+	          presence: true,
+	          format: {with: VALID_EMAIL_REGEX}
+
+	validates :password, 
+	          presence: true, 
+	          length: { minimum: 8, maximum: 32 },
+	          format: { with: VALID_PASSWORD_REGEX }
+
+
+	 has_secure_password
+end
