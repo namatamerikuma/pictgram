6e0dbd7782bff904e5bd9311366123d76c4b5224
namatamerikuma <lshengtianmu55@gmail.com>
2020-02-08 19:21:14 +0900

    No.20 task

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,10 @@ Rails.application.routes.draw do
   get 'pages/help'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :users
-
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
+
+  resources :users
+  resources :topics
 end
